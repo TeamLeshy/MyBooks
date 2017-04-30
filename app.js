a885540d13432239623199107bf7fa6fd555d551
dreadlocker <svetozariliev13@gmail.com>
2017-04-30 16:35:53 +0300

    Added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import router from 'router';
+
+window.onload = (() => {
+    if (!window.location.hash) {
+        window.location += '#/home';
+        window.location.reload();
+    }
+})();
+
