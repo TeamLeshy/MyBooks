a7c1d3674d1b886ede1b96d3a4e2d1e8c3988ac5
Svetozar Iliev <Svetozar Iliev>
2017-05-07 09:27:49 +0300

    Fixed PATHs in systemjs-config.js and src for it in index.html

--- Utils/config/systemjs-config.js
+++ Utils/config/systemjs-config.js
@@ -1,13 +1,12 @@
  SystemJS.config({
-            transpiler: 'plugin-babel',
-            map: {
-                'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
-                'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
-                'books': './Utils/books.js',
-                'templates': './templates/template.js',
-                'router': './Utils/router.js',
-                'app': './app.js',                
-            }
-        });
-   
-    
+     transpiler: 'plugin-babel',
+     map: {
+         'plugin-babel': '../../node_modules/systemjs-plugin-babel/plugin-babel.js',
+         'systemjs-babel-build': '../../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+         'books': '../../Utils/books.js',
+         'templates': '../../templates/template.js',
+         'router': '../../Utils/router.js',
+         'app': '../../app.js',
+     }
+ });
+ System.import('app');
